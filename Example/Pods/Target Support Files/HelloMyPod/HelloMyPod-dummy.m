2893c650e4ac4712d60a266a8c0da8cab3f91f67
liuchang.5 <liuchang.5@bytedance.com>
2020-11-19 18:05:37 +0800

    Init

---
+++ Example/Pods/Target Support Files/HelloMyPod/HelloMyPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HelloMyPod : NSObject
+@end
+@implementation PodsDummy_HelloMyPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-HelloMyPod_Example/Pods-HelloMyPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloMyPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HelloMyPod_Example
+@end
